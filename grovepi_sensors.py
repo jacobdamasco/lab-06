dc52973c990828a6c9b8d905dd13a89a3d510e7c
Jacob Damasco <damasco@usc.edu>
2023-02-28 16:36:01 -0800

    added lines for testing on pi

--- grovepi_sensors.py
+++ grovepi_sensors.py
@@ -3,6 +3,9 @@ import grovepi
 from grove_rgb_lcd import *
 
 if __name__ == "__main__":
+    # set the bus
+    grovepi.set_bus("RPI_1")
+
     # set rotary angle sensor to A0
     potentiometer = 0
 
@@ -22,6 +25,10 @@ if __name__ == "__main__":
             # measure distance (ultrasonic ranger)
             dist_sensor = grovepi.analogRead(sensor)
 
+
+            print("Threshold reading: " + threshold)
+            print("Sensor reading: " + dist_sensor)
+
             # check object is less than threshold
             if (dist_sensor <= threshold):
                 output = threshold + " OBJ PRES\n" + dist_sensor
@@ -30,6 +37,6 @@ if __name__ == "__main__":
                 output = threshold + '\n' + dist_sensor
                 setText(output)
         except:
-            grovepi.analogRead(potentiometer)
+            print("Error executing")
 
         time.sleep(0.1)
